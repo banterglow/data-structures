05b73aa4eb159b0df3573fdfb82089782c1b13a7
wongal5 <alberthwong2@gmail.com>
2017-11-29 15:46:00 -0800

    Completed Function Stack and Progress on Functional Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,8 @@
 var Queue = function() {
-  var someInstance = {};
+  var someInstance = {
+    top: -1,
+    bottom: 0
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,9 +10,20 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    someInstance[top] += 1;
+    storage[someInstance.top] = value;
   };
 
   someInstance.dequeue = function() {
+    var result;
+    
+    if (someInstance.top >= someInstance.bottom) {
+      result = storage[someInstance.bottom];
+      storage[someInstance.bottom] = undefined;
+      someInstance.bottom += 1;
+    }
+
+    return result;
   };
 
   someInstance.size = function() {
@@ -17,3 +31,5 @@ var Queue = function() {
 
   return someInstance;
 };
+
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,29 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {
+    top: -1
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance.top += 1;
+    storage[someInstance.top] = value;
   };
 
   someInstance.pop = function() {
+    var result;
+    if (someInstance.top >= 0) {
+      result = storage[someInstance.top];
+      storage[someInstance.top] = undefined;
+      someInstance.top -= 1;
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return someInstance.top + 1;
   };
 
   return someInstance;
