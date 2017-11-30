b7e918ccd5676cc0d0046624d825c7cbadf5c74b
wongal5 <alberthwong2@gmail.com>
2017-11-30 13:56:11 -0800

    completed tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,9 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,9 +12,32 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // make babyTree with value
+  var newTree = Tree(value);
+  // push babyTree to children array
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
+  // if curNode value eql target
+    // return true;
+  // else if curNode has children
+    // loop through all children
+      // recursively call contain on child and return result
+  // else
+    // return false;
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length !== 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+    return false;
+    // return false;
+  }
+
 };
 
 
