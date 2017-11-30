3edcbda8352fee2c39a76291235be0b48ac72254
wongal5 <alberthwong2@gmail.com>
2017-11-29 20:08:56 -0800

    add pseudocode for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -15,10 +15,26 @@ var LinkedList = function() {
   };
 
   list.removeHead = function() {
-    // 
+    // define result variable
+
+    // if head and tail are !null
+      // go to head
+      // store head node's value into variable
+      // set LL's head prop to curNode's next prop
+      // delete curNode
+
+    // return variable containing value
+
   };
 
   list.contains = function(target) {
+    // go to head
+    // if (check value) is true
+      // return true
+    // else
+      // if next is !null
+        // return list.contains(*next node*);
+      // else false
   };
 
   return list;
