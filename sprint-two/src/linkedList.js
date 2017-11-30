a3b797f393c5bd9bac846881dbdc5f47e219c7dd
wongal5 <alberthwong2@gmail.com>
2017-11-30 10:32:45 -0800

    completed linkedList and time complexity

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,8 +5,16 @@ var LinkedList = function() {
 
   list.addToTail = function(value) {
     // Create a new node instance
+    var newNode = Node(value);
     // if first node (head and tail null)
-      // set head and tail to new node obj
+    // set head and tail to new node obj
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
     // else
       // use LL to traverse to current tail
       // set current tail next to eql new node obj
@@ -16,25 +24,46 @@ var LinkedList = function() {
 
   list.removeHead = function() {
     // define result variable
+    var result, curNode;
 
     // if head and tail are !null
       // go to head
       // store head node's value into variable
       // set LL's head prop to curNode's next prop
       // delete curNode
-
+    if (this.head !== null) {
+      result = this.head.value;
+      curNode = this.head;
+      if (this.head.next === null) {
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.head = this.head.next;
+      }
+      delete curNode;
+    }
     // return variable containing value
-
+    // if becomes empty, set head and tail to null
+    return result;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, curNode) {
     // go to head
     // if (check value) is true
       // return true
+    curNode = curNode || this.head;
+    if (curNode.value === target) {
+      return true;
+    } else {
+      if (curNode.next === null) {
+        return false;
+      }
+      return this.contains(target, curNode.next);
+    }
     // else
-      // if next is !null
-        // return list.contains(*next node*);
-      // else false
+    //   if next is !null
+    //     return list.contains(*next node*);
+    //   else false
   };
 
   return list;
@@ -51,4 +80,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+addToTail: constant
+removeHead: constant
+contains: linear
  */
